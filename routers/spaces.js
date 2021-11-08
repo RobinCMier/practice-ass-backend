c9e7dcfb478d2f0f68fbef2a817449d96f2bd5ba
Itsapon <robintmier@gmail.com>
2021-11-08 20:30:45 +0100

    added endpoint for one space and all stories. Be aware that stories is nested

--- routers/spaces.js
+++ routers/spaces.js
@@ -1,19 +1,38 @@
 //imports
 const { Router } = require("express");
 const Space = require("../models").space;
+const Story = require("../models").story;
 
 //define routes
 const router = new Router();
 //ENDPOINTS
 
 //GET requests
+//get all spaces
 router.get("/", async (req, res) => {
   try {
     const allSpaces = await Space.findAll();
     console.log("requesting all spaces");
     res.send(allSpaces);
   } catch (e) {
-    console.log(`Spaces endpoint failed. Message: ${e}`);
+    console.log(`Allspaces endpoint failed. Message: ${e}`);
   }
 });
 module.exports = router;
+//get spacebypk and include all stories
+router.get("/:id", async (req, res) => {
+  try {
+    const spaceId = parseInt(req.params.id);
+    console.log("requesting one space and its stories..");
+    const oneSpace = await Space.findByPk(spaceId, {
+      include: [{ model: Story }],
+    });
+    if (!oneSpace) {
+      res.status(404).send("This space does not exist");
+    } else {
+      res.send(oneSpace);
+    }
+  } catch (e) {
+    console.log(`SpaceAndStories endpoint failed. Here's the message: ${e}`);
+  }
+});
